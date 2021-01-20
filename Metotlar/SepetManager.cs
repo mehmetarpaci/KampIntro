62b0078ad8ac7bb942bb4b37c6fc737d6502bccb
Mehmet Arpacı <arpaci_mehmet@outlook.com>
2021-01-20 23:50:40 +0200

    20 January

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urun.Adi);
+
+        }
+        
+        public void Ekle2(string urunAdi, string Aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urunAdi);
+
+        }
+
+
+    }
+}
